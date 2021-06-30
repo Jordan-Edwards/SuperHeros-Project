242fa9e262bb466e1c98cb1ebfb5517977cac394
Jordan-Edwards <edwardsjordan441@gmail.com>
2021-06-30 08:35:29 -0500

    Fixed continuity erros in all files / Added templates for Detail, Create, & Delete / 'change.html' not functioning

--- SuperHerosProject/SuperHerosapp/views.py
+++ SuperHerosProject/SuperHerosapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
-
-
 # Create your views here.
+
 
 def index(request):
     all_superheroes = Superhero.objects.all()
@@ -13,21 +12,12 @@ def index(request):
     }
     return render(request, 'SuperHerosapp/index.html', context)
 
-
-def create(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary_ability = request.POST.get('primary_ability')
-        secondary_ability = request.POST.get('secondary_ability')
-        catchphrase = request.POST.get('catchphrase')
-        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability,
-                                  secondary_ability=secondary_ability, catchphrase=catchphrase)
-        new_superhero.save()
-        return HttpResponseRedirect(reverse('SuperHerosapp:index'))
-    else:
-        return render(request, 'SuperHerosapp/create.html')
-
+def detail(request, superhero_id):
+    specific_superhero = Superhero.objects.get(pk=superhero_id)
+    context = {
+        'specific_superhero': specific_superhero
+    }
+    return render(request, 'SuperHerosapp/detail.html', context)
 
 def change(request, superhero_id):
     specific_superhero = Superhero.objects.get(pk=superhero_id)
@@ -40,23 +30,27 @@ def change(request, superhero_id):
         change_primary_ability = request.POST.get('change_primary_ability')
         change_secondary_ability = request.POST.get('change_secondary_ability')
         change_catchphrase = request.POST.get('change_catchphrase')
-        superhero_id = Superhero(name=change_name, alter_ego=change_alter_ego, primary_ability=change_primary_ability,
-                                 secondary_ability=change_secondary_ability, catchphrase=change_catchphrase)
+        superhero_id = Superhero(name=change_name, alter_ego=change_alter_ego, primary_ability=change_primary_ability, secondary_ability=change_secondary_ability, catchphrase=change_catchphrase)
         superhero_id.save()
         return HttpResponseRedirect(reverse('SuperHerosapp:detail', context))
     else:
         return render(request, 'SuperHerosapp/change.html', context)
 
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('SuperHerosapp:index'))
+    else:
+        return render(request, 'SuperHerosapp/create.html')
 
-def delete(superhero_id):
+
+def delete(request, superhero_id):
     delete_superhero = Superhero.objects.get(pk=superhero_id)
     Superhero.delete(delete_superhero)
     return HttpResponseRedirect(reverse('SuperHerosapp:index'))
-
-
-def detail(request, superhero_id):
-    specific_superhero = Superhero.objects.get(pk=superhero_id)
-    context = {
-        'specific_superhero': specific_superhero
-    }
-    return render(request, 'SuperHerosapp/detail.html', context)
