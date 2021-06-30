50f2d3d9c29ed0c9f24cc8405328c0b6cba6b55e
Jordan-Edwards <edwardsjordan441@gmail.com>
2021-06-29 20:34:49 -0500

    Conncected to SQL database / Created 'Superheros' class in 'models.py'

--- SuperHerosProject/SuperHerosProject/settings.py
+++ SuperHerosProject/SuperHerosProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SuperHerosapp.apps.SuperHerosappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'SuperHerosProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'Superhero_database',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
 
---
+++ SuperHerosProject/SuperHerosapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SuperHerosappConfig(AppConfig):
+    name = 'SuperHerosapp'
